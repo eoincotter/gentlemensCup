6625fa12c8da7a89dd46c4f3406a008cc96326b4
[eoin cotter] <[eoin369@gmail.com]>
2021-05-22 17:14:38 +0100

    remove hard coding of course data; create coure data dictionary

--- script.js
+++ script.js
@@ -3,28 +3,22 @@ var handicaps = {
   "Conall Hunt": 30
 };
 
+var courseData = {
+  'Glen Mill' : {'par' : [3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3], 'index' : [7, 5, 15, 11, 13, 17, 9, 1, 3, 2, 4, 18, 10, 12, 16, 14, 8, 6]},
+  // update lep indices
+  'Leopardstown' : {'par' : [3,4,3,4,4,4,4,5,4,4,4,4,3,4,3,4,3,4], 'index' : [7, 5, 15, 11, 13, 17, 9, 1, 3, 2, 4, 18, 10, 12, 16, 14, 8, 6]}
+  };
+
 function calculateTotals() {
-  var hole1 = Number(document.forms["score-input-1"]["score"].value);
-  var hole2 = Number(document.forms["score-input-2"]["score"].value);
-  var hole3 = Number(document.forms["score-input-3"]["score"].value);
-  var hole4 = Number(document.forms["score-input-4"]["score"].value);
-  var hole5 = Number(document.forms["score-input-5"]["score"].value);
-  var hole6 = Number(document.forms["score-input-6"]["score"].value);
-  var hole7 = Number(document.forms["score-input-7"]["score"].value);
-  var hole8 = Number(document.forms["score-input-8"]["score"].value);
-  var hole9 = Number(document.forms["score-input-9"]["score"].value);
-  var hole10 = Number(document.forms["score-input-10"]["score"].value);
-  var hole11 = Number(document.forms["score-input-11"]["score"].value);
-  var hole12 = Number(document.forms["score-input-12"]["score"].value);
-  var hole13 = Number(document.forms["score-input-13"]["score"].value);
-  var hole14 = Number(document.forms["score-input-14"]["score"].value);
-  var hole15 = Number(document.forms["score-input-15"]["score"].value);
-  var hole16 = Number(document.forms["score-input-16"]["score"].value);
-  var hole17 = Number(document.forms["score-input-17"]["score"].value);
-  var hole18 = Number(document.forms["score-input-18"]["score"].value);
-  var front9Score = hole1 + hole2 + hole3 + hole4 + hole5 + hole6 + hole7 + hole8 + hole9;
-  var back9Score = hole10 + hole11 + hole12 + hole13 + hole14 + hole15 + hole16 + hole17 + hole18;
+  var scores = []
+  for (i = 1; i < 19; i++) {
+    scores[scores.length] = Number(document.forms[`score-input-${i}`]["score"].value);
+  }
+
+  var front9Score = scores.slice(0,9).reduce((a, b) => a + b, 0);
+  var back9Score = scores.slice(9).reduce((a, b) => a + b, 0);
   var totalScore = front9Score + back9Score;
+
   document.getElementById("front-9-total").innerHTML = `${front9Score}`;
   document.getElementById("back-9-total").innerHTML = `${back9Score}`;
   document.getElementById("18-total").innerHTML = `${totalScore}`;
@@ -33,12 +27,30 @@ function calculateTotals() {
 }
 
 function processName() {
+  // process input
   var playerName = document.getElementById("names").value;
   var playerHandicap = handicaps[playerName];
+  var courseName = document.getElementById("courses").value;
+
+  // update detais
   document.getElementById("player-name").innerHTML = `Name: ${playerName}`;
   document.getElementById("player-handicap").innerHTML = `Handicap: ${playerHandicap}`;
+  document.getElementById("course-name").innerHTML = `Course: ${courseName}`;
+
+  // change display
   document.getElementById("player-details").style.display = "block";
   document.getElementById("player-name-confirmation").style.display = "none";
-  document.getElementById("glenmill-scorecard-entry").style.display = "block";
+  document.getElementById("scorecard-entry").style.display = "block";
+
+  // course details for scorecard display
+  for (i = 0; i < 18; i++) {
+    document.getElementById(`par-${i + 1}`).innerHTML = `${courseData[`${courseName}`]['par'][i]}`;
+    document.getElementById(`index-${i + 1}`).innerHTML = `${courseData[`${courseName}`]['index'][i]}`;
+  }
+
+  // add totals
+  document.getElementById('front-9-par').innerHTML = `${courseData[`${courseName}`]['par'].slice(0,9).reduce((a, b) => a + b, 0)}`;
+  document.getElementById('back-9-par').innerHTML = `${courseData[`${courseName}`]['par'].slice(9).reduce((a, b) => a + b, 0)}`;
+  document.getElementById('total-par').innerHTML = `${courseData[`${courseName}`]['par'].slice(0).reduce((a, b) => a + b, 0)}`;
 }
 
